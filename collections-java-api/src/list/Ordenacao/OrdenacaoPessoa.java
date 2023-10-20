32f299cbef6168def680dce97a3012c06308232e
Marchi8 <luandeoliveirasoares@gmail.com>
2023-10-20 19:35:06 -0300

    Resolvendo exercicios CatalogoLivros e OrdenacaoPessoa

---
+++ collections-java-api/src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,44 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    // Atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 22, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 24, 1.60);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 62, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 42, 1.90);
+
+        System.out.println(ordenacaoPessoa.pessoaList);
+
+        // System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        // System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+
+}
---
+++ collections-java-api/src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,67 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    // Atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livroPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livroPorAutor.add(l);
+                }
+            }
+        }
+        return livroPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livroPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livroPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livroPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2023);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2010);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2010);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2000);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2020);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2023));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 4"));
+    }
+}
